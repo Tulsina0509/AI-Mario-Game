909bfbc89393c97d727bca29a094d2c189f20add
Tulsina0509 <74660837+Tulsina0509@users.noreply.github.com>
2021-12-04 15:21:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
+img=**;
+noseX= 0;
+noseY=0;
+marioX=325;
+marioY=325
+
 function preload() {
+	img=loadImage("mario05.png")
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
 }
 
 function setup() {
+	createCanvas(650,400);
+	video=createCapture(VIDEO);
+	video.size(600,300);
 	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
 	instializeInSetup(mario);
+	
+	poseNet = nl5.poseNet(video,modelLoaded);
+	poseNet.on('pose',gotPoses);
 }
+
+function modelLoaded(){
+	console.log('Model Loaded!);
+		    }
 
 function draw() {
 	game()
