dd37e8033dc182e7c2fb2461282626381bec02ad
Tulsina0509 <74660837+Tulsina0509@users.noreply.github.com>
2021-12-04 15:44:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ img="";
 noseX= 0;
 noseY=0;
 marioX=325;
-marioY=325;`
+marioY=325;
 
 function preload() {
 	img=loadImage("mario05.png")
@@ -12,19 +12,20 @@ function preload() {
 }
 
 function setup() {
-	createCanvas(650,400);
-	video=createCapture(VIDEO);
-	video.size(600,300);
 	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
 	instializeInSetup(mario);
+	createCanvas(650,400);
+	video=createCapture(VIDEO);
+	video.size(600,300);
 	
-	poseNet = nl5.poseNet(video,modelLoaded);
+	
+	poseNet = ml5.poseNet(video,modelLoaded);
 	poseNet.on('pose',gotPoses);
 }
 
 function modelLoaded(){
-	console.log('Model Loaded!);
+	console.log('Model Loaded!');
 		    }
 		    
 function gotPoses(results)
