c627e2c22d6fbafef2bccde2f3dda53a22d649a9
Tulsina0509 <74660837+Tulsina0509@users.noreply.github.com>
2022-01-25 18:29:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -38,9 +38,10 @@ function gotPoses(results)
 {
 if(results.length >0)
 {
+console.log(results);	
 noseX = results[0].pose.nose.x;
 noseY = results[0].pose.nose.y;
-console.log("noseX = " + noseX +" , noseY = " + noseY);
+
 }
 }
 
