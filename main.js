504c51886359a14b7bc8079c4420603d07a70c0d
Tulsina0509 <74660837+Tulsina0509@users.noreply.github.com>
2022-01-25 18:17:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,13 @@ function preload() {
 function setup() {
 	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
+	
 	instializeInSetup(mario);
+	
+	video = createCapture(VIDEO);
+        video.size(800,400);
+	video.parent('game_console');
+	
 	createCanvas(650,400);
 	video=createCapture(VIDEO);
 	video.size(600,300);
